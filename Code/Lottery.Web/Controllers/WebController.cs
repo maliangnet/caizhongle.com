f5d1bd2491e44fa9ef3c2a0c6ccaa5d97a3de04d
MaLiang <maliangnet@126.com>
2013-11-07 17:26:51 +0800

    原型图

---
+++ Code/Lottery.Web/Controllers/WebController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lottery.Web.Controllers
+{
+    public class WebController : Controller
+    {
+        //
+        // GET: /Web/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
